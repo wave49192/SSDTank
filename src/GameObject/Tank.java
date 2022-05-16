a693144a0c6002de7be5be294f8dc18cac8b32d3
TopsonArcana <poomtum2544@hotmail.com>
2022-05-17 00:08:13 +0700

    Revert "Implement State design pattern into moving and image."
    This reverts commit 9dceb0c59398fbd3535829dc781e080103f82184.

--- src/GameObject/Tank.java
+++ src/GameObject/Tank.java
@@ -1,107 +1,54 @@
 package GameObject;
 
-import GameObject.state.State;
-
-import javax.swing.*;
-import java.awt.*;
-
 public class Tank extends WObject{
-	private State state;
-	private final int playerNumber;
-	public Tank(int x, int y, int playerNumber) {
+	public Tank(int x, int y) {
 		super(x, y);
-		state = new IdleState();
-		this.playerNumber = playerNumber;
 	}
 
-	public void turnNorth() { state = new TurnNorthState(); }
+	private int dx;
+	private int dy;
 
-	public void turnSouth() { state = new TurnSouthState(); }
+	public void turnNorth() {
+		dx = -1;
+		dy = 0;
+	}
 
-	public void turnWest() { state = new TurnWestState(); }
+	public void turnSouth() {
+		dx = 1;
+		dy = 0;
+	}
 
-	public void turnEast() { state = new TurnEastState(); }
+	public void turnWest() {
+		dx = 0;
+		dy = -1;
+	}
 
-	public void stop() { state = new IdleState(); }
+	public void turnEast() {
+		dx = 0;
+		dy = 1;
+	}
 
 	public void move() {
-		setX(getX() + state.getDx());
-		setY(getY() + state.getDy());
+		setX(getX() + dx);
+		setY(getY() + dy);
 	}
 
-	public boolean checkState(String stateName) {
-		return state.getClass().getName().equals("GameObject.Tank$" + stateName);
+	public void stop() {
+		dx = 0;
+		dy = 0;
 	}
 
-	public boolean isMoveNorth() { return checkState("TurnNorthState"); }
+	public int getDx() { return dx; }
 
-	public boolean isMoveSouth() { return checkState("TurnSouthState"); }
+	public int getDy() { return dy; }
 
-	public boolean isMoveWest() { return checkState("TurnWestState"); }
+	public boolean isMoveNorth() { return dx == -1 && dy == 0; }
 
-	public boolean isMoveEast() { return checkState("TurnEastState"); }
+	public boolean isMoveSouth() { return dx == 1 && dy == 0; }
 
-	public boolean isIdle() { return checkState("IdleState"); }
+	public boolean isMoveWest() { return dx == 0 && dy == -1; }
 
-	public State getState() { return state; }
+	public boolean isMoveEast() { return dx == 0 && dy == 1; }
 
-	public class TurnNorthState extends State {
-		public TurnNorthState() {
-			setDx(-1);
-			setDy(0);
-			if (playerNumber == 1) {
-				setImage(new ImageIcon("images/TankNorth1.png").getImage());
-			} else if (playerNumber == 2) {
-				setImage(new ImageIcon("images/TankNorth2.png").getImage());
-			}
-		}
-	}
-
-	public class TurnSouthState extends State {
-		public TurnSouthState() {
-			setDx(1);
-			setDy(0);
-			if (playerNumber == 1) {
-				setImage(new ImageIcon("images/TankSouth1.png").getImage());
-			} else if (playerNumber == 2) {
-				setImage(new ImageIcon("images/TankSouth2.png").getImage());
-			}
-		}
-	}
-
-	public class TurnWestState extends State {
-		public TurnWestState() {
-			setDx(0);
-			setDy(-1);
-			if (playerNumber == 1) {
-				setImage(new ImageIcon("images/TankWest1.png").getImage());
-			} else if (playerNumber == 2) {
-				setImage(new ImageIcon("images/TankWest2.png").getImage());
-			}
-		}
-	}
-
-	public class TurnEastState extends State {
-		public TurnEastState() {
-			setDx(0);
-			setDy(1);
-			if (playerNumber == 1) {
-				setImage(new ImageIcon("images/TankEast1.png").getImage());
-			} else if (playerNumber == 2) {
-				setImage(new ImageIcon("images/TankEast2.png").getImage());
-			}
-		}
-	}
-
-	public class IdleState extends State {
-		public IdleState() {
-			setDx(0);
-			setDy(0);
-			if (playerNumber == 1) {
-				setImage(new ImageIcon("images/TankNorth1.png").getImage());
-			} else if (playerNumber == 2) {
-				setImage(new ImageIcon("images/TankNorth2.png").getImage());
-			}
-		}
-	}
+	public boolean isIdle() { return dx == 0 && dy == 0; }
 }
