67783a6e113fa2dbbe022f6fb137a29778ff977f
TopsonArcana <poomtum2544@hotmail.com>
2022-05-18 18:12:21 +0700

    Move ai to board.

--- src/main/Game.java
+++ src/main/Game.java
@@ -25,7 +25,6 @@ public class Game extends JFrame {
 	private Board board;
 	private final int boardSize = 20;
 	private final int barSize = 0;
-	private AI ai;
 
 	public Game() {
 		Controller controller = new Controller();
@@ -36,14 +35,13 @@ public class Game extends JFrame {
 //		tankAudioController = new TankAudioController(board.getPlayerTanks().get(0));
 //		tankAudioController.initialSound();
 		gridUI = new GridUI();
-		ai = new AI(board.getEnemyTanks(), board, "NoLookingBackwardStrategy");
 		Thread thread = new Thread() {
 			@Override
 			public void run() {
 				while (!board.getIsOver()) {
 					gridUI.repaint();
 					moving();
-					ai.executeStrategy();
+					board.getAi().executeStrategy();
 					setDeadTanks();
 //					tankAudioController.playTankMovementSound();
 					try {
